74645bb9c99c9eacf938af176a87f092932a5f87
tharushasamarawickrama <tharushathathsaraviduranga@gmail.com>
2024-06-14 20:29:21 +0530

    create pages and routes

--- Mern/client/src/App.jsx
+++ Mern/client/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
 
-export default function sachith() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+
+
       </Routes>
     </BrowserRouter>
   )
